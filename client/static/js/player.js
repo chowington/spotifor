6f16d8ebf818e91841b67a36209e5ead604741ec
Connor Howington <connor.howington@gmail.com>
2019-09-02 17:57:34 -0400

    Begin implementing functionality in Vue

--- client/static/js/player.js
+++ client/static/js/player.js
@@ -7,37 +7,32 @@ $(function() {
       name: 'Web Playback SDK Quick Start Player',
       getOAuthToken: cb => { cb(token); }
     });
-  
+
     // Error handling
     player.addListener('initialization_error', ({ message }) => { console.error(message); });
     player.addListener('authentication_error', ({ message }) => { console.error(message); });
     player.addListener('account_error', ({ message }) => { console.error(message); });
     player.addListener('playback_error', ({ message }) => { console.error(message); });
-  
+
     // Playback status updates
     player.addListener('player_state_changed', state => {
-      console.log(state);
-      if(state.paused) {
-        $('#play-button').text('Play');
-      } else {
-        $('#play-button').text('Pause');
-      }
+      app.paused = state.paused;
     });
-  
+
     // Ready
     player.addListener('ready', ({ device_id }) => {
       console.log('Ready with Device ID', device_id);
       $('#status').text('Connected!');
       $('#play-button').prop('disabled', false);
     });
-  
+
     // Not Ready
     player.addListener('not_ready', ({ device_id }) => {
       console.log('Device ID has gone offline', device_id);
       $('#status').text('Not connected.');
       $('#play-button').prop('disabled', true);
     });
-  
+
     // Connect to the player!
     player.connect();
   };
@@ -46,4 +41,3 @@ $(function() {
 function togglePlay() {
   player.togglePlay();
 };
-
