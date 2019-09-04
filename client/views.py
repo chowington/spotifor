a37657c3860ef7f41b74126fb8cc5bd5315bce25
Connor Howington <connor.howington@gmail.com>
2019-09-04 00:51:50 -0400

    Check for valid session

--- client/views.py
+++ client/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from urllib.parse import urlencode
+from uuid import uuid4
 
 # Create your views here.
 def login_view(request):
@@ -12,11 +13,15 @@ def login_view(request):
         'playlist-read-private',
     ]
 
+    session_id = str(uuid4())
+    request.session['session_id'] = session_id
+
     payload = {
         'client_id': '1f9f77385be84819a18e2af962f839ba',
         'response_type': 'code',
         'redirect_uri': 'https://chowington.pythonanywhere.com/spotifor/client',
         'scope': ' '.join(scopes),
+        'state': session_id
     }
 
     url = 'https://accounts.spotify.com/authorize?' + urlencode(payload)
@@ -24,4 +29,8 @@ def login_view(request):
     return HttpResponseRedirect(url)
 
 def client_view(request):
-    return render(request, 'client/client.html')
+    if request.GET['session_id'] == request.session['session_id']:
+        request.session.flush()
+        return render(request, 'client/client.html')
+    else:
+        return HttpResponse('Invalid request')
