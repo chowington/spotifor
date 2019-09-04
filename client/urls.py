10977806534246837e1998ca0c4d880825a4910a
Connor Howington <connor.howington@gmail.com>
2019-09-04 00:18:44 -0400

    Begin implementing Spotify login

--- client/urls.py
+++ client/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.player, name='player'),
+    path('', views.login_view, name='login'),
+    path('client/', views.client_view, name='client'),
 ]
-
--- client/views.py
+++ client/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from urllib.parse import urlencode
 
 # Create your views here.
-def player(request):
-    return render(request, 'client/player.html')
+def login_view(request):
+    scopes = [
+        'playlist-read-collaborative',
+        'playlist-modify-private',
+        'streaming',
+        'playlist-modify-public',
+        'playlist-read-private',
+    ]
 
+    payload = {
+        'client_id': '1f9f77385be84819a18e2af962f839ba',
+        'response_type': 'code',
+        'redirect_uri': 'https://chowington.pythonanywhere.com/spotifor/client',
+        'scope': ' '.join(scopes),
+    }
+
+    url = 'https://accounts.spotify.com/authorize?' + urlencode(payload)
+
+    return HttpResponseRedirect(url)
+
+def client_view(request):
+    return render(request, 'client/client.html')
